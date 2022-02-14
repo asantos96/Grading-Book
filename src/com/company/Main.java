9026258b0caf7e8ca50fa18b4e95b43e70fd8a88
Andrea Santos <andreasantos@andreas-mbp.myfiosgateway.com>
2022-02-14 16:09:01 -0500

    initial Grading Book start

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int studentGrade = 70 ;
+    if (studentGrade >=90) {
+        System.out.println ("You got an A!");
+    }else if (studentGrade >=80) {
+        System.out.println ("You got a B!");
+    }else if (studentGrade >=70) {
+        System.out.println ("You got a C!");
+    }else if (studentGrade >=60) {
+        System.out.println ("You got a D!");
+    } else {
+        System.out.println ("You failed.");
+    }
+    }
+}
